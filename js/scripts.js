740905b22c7d7f504641902f235e1b89dc1ed5f1
Crystal Fecteau and Stephen Trewick <crystal@>
2019-04-02 13:47:09 -0700

    show form prompting a phrase and returns the phrase replacing the vowels with -

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+  $("form.wordPuzzle").submit(function(event){
+    event.preventDefault();
+    var string = $("input#string").val();
+    var array = string.split('');
+    console.log(array);
+
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] ==='a' || array[i] ==='e' || array[i] ==='i' || array[i] === 'o' || array[i] ==='u') {
+        array[i] = '-';
+
+        console.log(array);
+      }
+      array.toString();
+    }
+
+      $("#formOutput").text(array);
+
+
+});
+
+});
